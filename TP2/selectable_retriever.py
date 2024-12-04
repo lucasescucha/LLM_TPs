0c1f28119634b13aa935b4b38ae4fb083e75bfc3
Lucas Matias Barrera <lucasmatiasbarrera@gmail.com>
2024-12-04 19:41:09 +0100

    Minor refactors

--- TP2/selectable_retriever.py
+++ TP2/selectable_retriever.py
@@ -10,8 +10,28 @@ from pinecone import Pinecone
 
 from pydantic import BaseModel, PrivateAttr
 
+
 class SelectableRetriever(BaseRetriever, BaseModel):
     _retrievers: Dict[str, BaseRetriever] = PrivateAttr({})
+
+    @staticmethod
+    def __create_vector_store(index: str) -> PineconeVectorStore:
+        return PineconeVectorStore(
+            pinecone_api_key=PINECONE_API_KEY,
+            index_name=index,
+            embedding=EMBEDDINGS_MODEL,
+            namespace=PINECONE_NAMESPACE_NAME,
+        )
+
+    @staticmethod
+    def __get_index_from_prompt(prompt: str) -> str:
+        prompt_lower = prompt.lower()
+        if 'lucas' in prompt_lower or 'barrera' in prompt_lower:
+            return "lucas-barrera"
+        elif 'gloria' in prompt_lower or 'gonzalez' in prompt_lower or 'gonzález' in prompt_lower:
+            return "gloria-gonzalez"
+        else:
+            raise KeyError()
 
     def __init__(self, pinecone_api: Pinecone) -> object:
         super().__init__()
@@ -20,28 +40,11 @@ class SelectableRetriever(BaseRetriever, BaseModel):
                    for index_info in pinecone_api.list_indexes()]
 
         for index in indexes:
-            vector_store = PineconeVectorStore(
-                pinecone_api_key=PINECONE_API_KEY,
-                index_name=index,
-                embedding=EMBEDDINGS_MODEL,
-                namespace=PINECONE_NAMESPACE_NAME,
-            )
-
+            vector_store = SelectableRetriever.__create_vector_store(index)
             self._retrievers[index] = vector_store.as_retriever()
 
     def _get_relevant_documents(self, query: str, *, run_manager) -> list[Document]:
-        def get_person_name_from_prompt(prompt: str) -> str:
-            prompt_lower = prompt.lower()
-            if 'lucas' in prompt_lower or 'barrera' in prompt_lower:
-                return "lucas-barrera"
-            elif 'gloria' in prompt_lower or 'gonzalez' in prompt_lower or 'gonzález' in prompt_lower:
-                return "gloria-gonzalez"
-            else:
-                raise KeyError()
-
-        person_name = get_person_name_from_prompt(query)
-        print("Retrieving documents for", person_name)
-        return self._retrievers[person_name]._get_relevant_documents(query=query, run_manager=run_manager)
-
-    def get_loaded_retrievers(self) -> Dict[str, BaseRetriever]:
-        return self._retrievers
+        index_name = SelectableRetriever.__get_index_from_prompt(query)
+        print("Retrieving documents for", index_name)
+        return self._retrievers[index_name]._get_relevant_documents(
+            query=query, run_manager=run_manager)
